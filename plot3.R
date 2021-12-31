5ad7e7c681ea034e37eb63e0509e535656c1070e
Tobi Akinmusuru <91935741+Takinmu1@users.noreply.github.com>
2021-12-30 20:39:30 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -25,6 +25,5 @@ with(data1, {
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 png("plot3.png", width=480, height=480)
-dev.off()
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
